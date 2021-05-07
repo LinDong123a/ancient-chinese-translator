bb1e36b4d87c6137fb2f00011c7f46081ce550b7
lds <lds354449275@gmail.com>
2021-05-07 08:07:41 +0800

    fix: rename inferece.py

--- inference.py
+++ inference.py
@@ -27,7 +27,12 @@ if __name__ == "__main__":
         src_vocab=src_tokenizer.vocab,
         trg_vocab=trg_vocab,
         lr=0.01, num_epoch=100, steps_per_epoch=20,
-        model_config={"embedding_dim": args.embedding_dim, "enc_hidden_dim": args.enc_hidden_dim, "dec_hidden_dim": args.dec_hidden_dim, "dropout": 0},
+        model_config={
+            "embedding_dim": args.embedding_dim,
+            "enc_hidden_dim": args.enc_hidden_dim,
+            "dec_hidden_dim": args.dec_hidden_dim,
+            "dropout": 0,
+        },
     )
 
     model = model.eval()
@@ -41,4 +46,3 @@ if __name__ == "__main__":
         )[0]
 
         print(res_sent)
-
--- inference.sh
+++ inference.sh
@@ -1,1 +1,1 @@
-python inferece.py --checkpoint_path models/Untitled/AN-159/checkpoints/epoch=64-step=48748.ckpt --src_vocab_path /home/featurize/data/dataset/src_vocab.json --trg_vocab_path /home/featurize/data/dataset/trg_vocab.json --embedding_dim 128 --enc_hidden_dim 256 --dec_hidden_dim 256
+python inference.py --checkpoint_path models/Untitled/AN-159/checkpoints/epoch=64-step=48748.ckpt --src_vocab_path /home/featurize/data/dataset/src_vocab.json --trg_vocab_path /home/featurize/data/dataset/trg_vocab.json --embedding_dim 128 --enc_hidden_dim 256 --dec_hidden_dim 256
