2bc52164f18a8c95134779172243224a0335dd5c
lds <lds354449275@gmail.com>
2021-05-14 08:49:36 +0800

    feat: support token type

--- inference.py
+++ inference.py
@@ -2,13 +2,14 @@ import argparse
 
 import torch
 
-from data.tokenization import CharTokenizer
+from data.tokenization import CharTokenizer, VernacularTokenTokenizer
 from data.vocab import Vocab
 from model import ModelInterface
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint_path", type=str, required=True)
+    parser.add_argument("--token_type", type=str, default="char", choices=["char", "token"])
     parser.add_argument("--src_vocab_path", type=str, required=True, help="白话文词表路径")
     parser.add_argument("--trg_vocab_path", type=str, required=True, help="文言文词表路径")
 
@@ -16,7 +17,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    src_tokenizer = CharTokenizer()
+    if args.token_type == "char":
+        src_tokenizer = CharTokenizer()
+    elif args.token_type == "token":
+        src_tokenizer = VernacularTokenTokenizer()
+
     src_tokenizer.load_vocab(args.src_vocab_path)
 
     trg_vocab = Vocab()
